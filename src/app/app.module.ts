8e4ae0c433c268c5f9fae78a40fbfb4f35437f69
Sayanaann <alexsayana@gmail.com>
2022-11-25 08:32:58 +0530

    nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,42 @@ import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 import { FoodItemsComponent } from './food-items/food-items.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const myRoute:Routes=[
+  {
+    path :"",
+    component :SignupComponent
+  },
+  {
+    path :"login",
+    component :LoginComponent
+  },
+  {
+    path :"fooditems",
+    component :FoodItemsComponent
+  },
+  {
+    path :"contact",
+    component :ContactUsComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
     LoginComponent,
     FoodItemsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
