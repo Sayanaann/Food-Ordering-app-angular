909376cedb2ded57942934ef30a6b30c981f243b
Sayanaann <alexsayana@gmail.com>
2022-11-25 08:18:22 +0530

    all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { FoodItemsComponent } from './food-items/food-items.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent
+    SignupComponent,
+    LoginComponent,
+    FoodItemsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
